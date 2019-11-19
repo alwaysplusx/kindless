0412d4d8b1872cdcaaed19d8b7e27f327cef024a
wuxii <wuxii@foxmail.com>
2019-11-19 13:41:51 +0800

    add spring-boot-admin

---
+++ monitors/kindless-monitor-admin/src/main/java/com/kindless/monitor/admin/AdminApplication.java
@@ -0,0 +1,20 @@
+package com.kindless.monitor.admin;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author wuxin
+ */
+@EnableDiscoveryClient
+@EnableAdminServer
+@SpringBootApplication
+public class AdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminApplication.class, args);
+    }
+
+}
