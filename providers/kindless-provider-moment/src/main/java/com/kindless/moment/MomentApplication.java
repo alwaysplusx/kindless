591790de31b4d7cced48ef174c277b3a7b4cf5b6
wuxii <wuxii@foxmail.com>
2019-12-06 17:06:50 +0800

    graphql example test code

--- providers/kindless-provider-moment/src/main/java/com/kindless/moment/MomentApplication.java
+++ providers/kindless-provider-moment/src/main/java/com/kindless/moment/MomentApplication.java
@@ -1,21 +1,9 @@
 package com.kindless.moment;
 
 import com.harmony.umbrella.data.repository.support.QueryableRepositoryFactoryBean;
-import com.kindless.moment.domain.Moment;
-import graphql.schema.GraphQLSchema;
-import graphql.schema.idl.RuntimeWiring;
-import graphql.schema.idl.SchemaGenerator;
-import graphql.schema.idl.SchemaParser;
-import graphql.schema.idl.TypeDefinitionRegistry;
-import graphql.schema.idl.TypeRuntimeWiring.Builder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.core.io.DefaultResourceLoader;
-import org.springframework.core.io.Resource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-
-import java.io.IOException;
 
 /**
  * @author wuxii
@@ -26,27 +14,6 @@ public class MomentApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MomentApplication.class, args);
-    }
-
-    @Bean
-    public GraphQLSchema graphQLSchema() throws IOException {
-        DefaultResourceLoader loader = new DefaultResourceLoader();
-        Resource resource = loader.getResource("classpath:graphql/moment.graphql");
-        TypeDefinitionRegistry typeRegistry = new SchemaParser().parse(resource.getFile());
-        RuntimeWiring wiring = buildRuntimeWiring();
-        return new SchemaGenerator()
-                .makeExecutableSchema(typeRegistry, wiring);
-    }
-
-    private RuntimeWiring buildRuntimeWiring() {
-        return RuntimeWiring.newRuntimeWiring()
-                .type("Query", this::applyDataFetcher)
-                .build();
-    }
-
-    private Builder applyDataFetcher(Builder builder) {
-        builder.dataFetcher("moment", environment -> new Moment());
-        return builder;
     }
 
 }
